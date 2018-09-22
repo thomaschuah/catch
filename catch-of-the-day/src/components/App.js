62e486a1ca60da372a6cbc0e9f61cfd00861695c
Thomas Chuah <thomas.chuah@o2ebrands.com>
2018-09-21 17:23:21 -0700

    More files

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from '../sample-fishes';
+import Fish from "./Fish";
 
 class App extends React.Component {
 
@@ -13,7 +15,7 @@ class App extends React.Component {
 	};
 	constructor(props){
 		super(props);
-	}
+	};
 
 	addFish = (fish) => {
 		// an object spread
@@ -26,14 +28,29 @@ class App extends React.Component {
 		this.setState({
 			fishes: fishes
 		});
-	}
+	};
+	loadSampleFishes = () => {
+		this.setState({
+			fishes: sampleFishes
+		});
+	};
 	render() {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market" age={100} />
+					<ul className="fishes">
+					{Object.keys(this.state.fishes).map(key => (
+		              <Fish
+		                key={key}
+		                index={key}
+		                details={this.state.fishes[key]}
+		                addToOrder={this.addToOrder}
+		              />
+		            ))}
+		            </ul>
 					<Order />
-					<Inventory addFish={this.addFish} />
+					<Inventory loadSampleFishes={this.loadSampleFishes} addFish={this.addFish} />
 				</div>
 
 			</div>
